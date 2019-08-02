041e7a727079fd884a4c24d3a1b40266f683fbbb
Jon <wintersfile@gmail.com>
2019-08-01 21:53:14 -0700

    client setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const passport = require("passport");
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 7770;
+app.listen(PORT);
